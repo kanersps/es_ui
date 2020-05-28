103e666c63d518a4d1e42dd17d0dbcd44c8651c4
Yagiz ALP <yagizwasmeow@gmail.com>
2020-05-28 16:18:53 +0300

    Replace __resource.lua with fxmanifest.lua

--- __resource.lua
+++ __resource.lua
@@ -1,12 +1,12 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 description 'es_ui by Kanersps.'
 
 ui_page 'ui.html'
 
-client_scripts {
-	'client.lua'
-}
+client_script 'client.lua'
 
 -- NUI Files
 files {
